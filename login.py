23154fd138fda47b7652cda50ac1523435f45d68
Ryan Ingraham <ryanisda1@outlook.com>
2022-09-18 12:40:47 -0400

    Updated login functions to be more compact

--- login.py
+++ login.py
@@ -14,39 +14,19 @@ def username_is_unique(username_input):
   return True
   
 def password_is_too_short(password):
-  if len(password) < 8:
-    return True
-
-  else:
-    return False
+  return len(password) < 8    
 
 def password_is_too_long(password):
-  if len(password) > 12:
-    return True
-
-  else:
-    return False
-
+  return len(password) > 12
+    
 def password_contains_uppercase_letter(password):
-  if any(char.isupper() for char in password):
-    return True
-
-  else:
-    return False
-
+  return any(char.isupper() for char in password)
+    
 def password_contains_number(password):
-  if any(char.isdigit() for char in password):
-    return True
-
-  else:
-    return False
-
+  return any(char.isdigit() for char in password)
+    
 def password_contains_special_char(password):
   valid_char = {'-', '_', '.', '!', '@', '#', '$', '^', '&', '(', ')'}
   
-  if any(char in valid_char for char in password):
-    return True
-
-  else:
-    return False
-      
+  return any(char in valid_char for char in password)
+    
