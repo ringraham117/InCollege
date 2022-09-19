3df22cbfc90c5b2fad4c92736615455e3938ec59
Ryan Ingraham <ryanisda1@outlook.com>
2022-09-19 18:39:15 -0400

    Copied files from Replit to store progress

--- state.py
+++ state.py
@@ -18,15 +18,17 @@ def goto_start_menu_state():
         print("Invalid selection. Please try again.")
         goto_start_menu_state()
 
+
 def goto_logging_in_state():
 
-    print("\n\nLogin:")
+    print("\nLogin:")
     username = input("Username: ")
     password = input("Password: ")
 
     if not db.user_exists_in_db(username, password):
-        print("\nIncorrect username/password, please try again.")
+        print("Incorrect username/password, please try again.")
         goto_logging_in_state()
+        return
 
     print("\nYou have successfully logged in.")
     goto_logged_in_state()
@@ -38,19 +40,13 @@ def goto_logged_in_state():
     user_input = input("\nEnter a selection: ")
 
     if menu.user_chose_to_find_job(user_input):
-        print("\nUnder construction.")
-        goto_logged_in_state()
+        print("\nUnder Construction.")
 
     elif menu.user_chose_to_find_someone(user_input):
-        print("\nUnder construction.")
-        goto_logged_in_state()
+        print("\nUnder Construction.")
 
     elif menu.user_chose_to_learn_a_skill(user_input):
         goto_learn_a_skill_state()
-
-    else:
-        print("\nInvalid selection.")
-        goto_logged_in_state()
 
 
 def goto_learn_a_skill_state():
@@ -59,30 +55,22 @@ def goto_learn_a_skill_state():
 
     if menu.user_chose_to_learn_web_dev(user_input):
         print("\nUnder construction.")
-        goto_learn_a_skill_state()
 
     elif menu.user_chose_to_learn_coding(user_input):
         print("\nUnder construction.")
-        goto_learn_a_skill_state()
 
     elif menu.user_chose_to_learn_communication(user_input):
         print("\nUnder construction.")
-        goto_learn_a_skill_state()
 
     elif menu.user_chose_to_learn_resume_critique(user_input):
         print("\nUnder construction.")
-        goto_learn_a_skill_state()
-    
+
     elif menu.user_chose_to_learn_excel(user_input):
         print("\nUnder construction.")
-        goto_learn_a_skill_state()
 
     elif menu.user_chose_to_goto_top_level_menu(user_input):
-        goto_logged_in_state()        
+        goto_logged_in_state()
 
-    else:
-        print("\nInvalid selection")
-        goto_learn_a_skill_state()
 
 def goto_create_new_account_state():
 
@@ -91,32 +79,34 @@ def goto_create_new_account_state():
     password = input("Enter a password: ")
 
     if not db.can_add_more_users():
-        print("All permitted accounts have been created, please come back later.")
-        goto_start_menu_state()
+        print(
+            "All permitted accounts have been created, please come back later."
+        )
+        return
 
-    elif not login.username_is_unique(username):
+    elif not login.username_is_unique(username, db.get_database()):
         print("That username is not available.")
-        goto_start_menu_state()
+        return
 
     elif login.password_is_too_short(password):
         print("Password is too short.")
-        goto_start_menu_state()
+        return
 
     elif login.password_is_too_long(password):
         print("Password is too long.")
-        goto_start_menu_state()
+        return
 
     elif not login.password_contains_uppercase_letter(password):
         print('Password must have at least one uppercase letter.')
-        goto_start_menu_state()
+        return
 
     elif not login.password_contains_number(password):
         print("Password must have at least one numeral.")
-        goto_start_menu_state()
+        return
 
     elif not login.password_contains_special_char(password):
         print("Password must have atleast one special character.")
-        goto_start_menu_state()
+        return
 
     db.add_user_to_db(username, password)
     print("Account successfully created.")
