a4bdb849c057b67aec8f719ec8987c01ea5efdad
Ryan Ingraham <ryanisda1@outlook.com>
2022-09-19 19:54:51 -0400

    Compressed menu functions, added return to state transitions, and added an else statement to replace multiple returns.

--- menu.py
+++ menu.py
@@ -1,4 +1,4 @@
-  def print_start_menu():
+def print_start_menu():
 
     print("\nInCollege")
     print("---------")
@@ -25,69 +25,29 @@ def print_skills_menu():
     print("6. Return to Previous Screen")
 
 def user_chose_to_login(user_input):
-    if user_input == "1":
-        return True
-
-    else:
-        return False
-
-
+    return user_input == "1"
+        
 def user_chose_to_create_new_account(user_input):
-    if user_input == "2":
-        return True
-
-    else:
-        return False
-
+    return user_input == "2"
 
 def user_chose_to_find_job(user_input):
-    if user_input == "1":
-        return True
-
-    else:
-        return False
-
+    return user_input == "1"        
 
 def user_chose_to_find_someone(user_input):
-    if user_input == "2":
-        return True
-
-    else:
-        return False
-
-
+    return user_input == "2"
+        
 def user_chose_to_learn_a_skill(user_input):
-    if user_input == "3":
-        return True
-
-    else:
-        return False
-
-
+    return user_input == "3"
+        
 def user_chose_to_learn_web_dev(user_input):
-    if user_input == "1":
-        return True
-
-    else:
-        return False
-
+    return user_input == "1"
 
 def user_chose_to_learn_coding(user_input):
-    if user_input == "2":
-        return True
-
-    else:
-        return False
-
-
+    return user_input == "2"
+    
 def user_chose_to_learn_communication(user_input):
-    if user_input == "3":
-        return True
-
-    else:
-        return False
-
-
+    return user_input == "3"
+       
 # To do:
 # Return true if user input is 4
 def user_chose_to_learn_resume_critique(user_input):
